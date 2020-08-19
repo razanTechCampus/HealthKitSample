f53a84d7faa1b93f890a69be1c65588dad726a36
RazanAlAli <razan.techcampus@gmail.com>
2020-08-19 20:21:36 +0300

    Initial Commit

---
+++ HealthTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthTest
+//
+//  Created by TechCampus on 8/19/20.
+//  Copyright © 2020 TechCampus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
